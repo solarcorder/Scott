6f920bffb792827af8207d36fe99fc75d1bf805c
solarcorder <sanketpareek27@gmail.com>
2026-04-06 12:59:30 +0530

    Update study.js

--- study.js
+++ study.js
@@ -1,4 +1,3 @@
 function openStudyTracker() {
-  document.getElementById("dashboard-overlay").style.display = "block";
-  document.getElementById("dashboard-frame").src = "study_tracker.html";
+  alert("clicked");
 }
